8865a124831393b9b1db5da4ce203195920bf308
ibernal-git <hola@ibernal.net>
2023-11-22 11:43:25 +0100

    feat: New use cases for the new menu options

--- utils/filter.py
+++ utils/filter.py
@@ -1,9 +1,19 @@
 from utils.constants import *
-# Construccion de filtro para la consulta a Clearpass
-def get_filter(macs_array, filter_type):
 
-  if FILTER.get(filter_type) == None:
-    print("Tipo de filtro no encontrado")
-    return
-  macs_string = ",".join('"{0}"'.format(elem) for elem in macs_array)
-  return '{"%s":[%s]}' % (FILTER[filter_type], macs_string)
+
+def get_filter(macs_array, filter_type, optional_filter=None):
+    filter_mapping = {
+        LOCAL_USERS: '{"%s":[%s]}'
+        % (FILTER[LOCAL_USERS], ",".join('"{0}"'.format(elem) for elem in macs_array)),
+        ENDPOINTS: '{"%s":[%s]}'
+        % (FILTER[ENDPOINTS], ",".join('"{0}"'.format(elem) for elem in macs_array)),
+        NAS_SESSION: '{"acctstoptime":{"$exists": false},"nasipaddress":"%s"}'
+        % optional_filter,
+        ACTIVE_SESSION: '{"acctstoptime":{"$exists": false}, "%s":[%s]}'
+        % (FILTER[ENDPOINTS], ",".join('"{0}"'.format(elem) for elem in macs_array)),
+    }
+
+    try:
+        return filter_mapping[filter_type]
+    except KeyError:
+        raise ValueError(f"Tipo de filtro no encontrado: {filter_type}")
