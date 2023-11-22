a08b0a4c329974b0769e2a244cac475358c3bc03
ibernal-git <hola@ibernal.net>
2023-11-22 11:41:56 +0100

    feat: Add new constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,14 @@
 # Nombre de archivo con las macs
-FILE_NAME = 'mac.txt'
-
+FILE_NAME = "mac.txt"
+DETAILS_FILE_NAME = "details.json"
 LOCAL_USERS = "local_users"
 ENDPOINTS = "endpoints"
-ENDPOINT_DETAIL = "endpoint_detail"
+ACTIVE_SESSION = "active_session"
+NAS_SESSION = "nas_session"
+CALLING_STATION_ID = "callingstationid"
 FILTER = {
     LOCAL_USERS: "user_id",
     ENDPOINTS: "mac_address",
+    ACTIVE_SESSION: "active_session",
+    NAS_SESSION: "nasipaddress",
 }
